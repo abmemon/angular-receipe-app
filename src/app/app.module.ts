ab7974063828daf85d169051e6b22ed81c39811a
Ahmed Bakhsh Memon <faysalmemon153@gmail.com>
2023-01-15 11:09:47 +0500

    Initial app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
+import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
+import { ReceipeItemComponent } from './receipes/receipe-list/receipe-item/receipe-item.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingListEditComponent } from './shoping-list/shoping-list-edit/shoping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReceipesComponent,
+    ReceipeListComponent,
+    ReceipeDetailComponent,
+    ReceipeItemComponent,
+    ShopingListComponent,
+    ShopingListEditComponent
   ],
   imports: [
     BrowserModule,
